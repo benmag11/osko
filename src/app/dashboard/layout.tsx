5a11c4f4b686b88c01c5b57b05b1da575866f468
benmag11 <benmaguire1111@gmail.com>
2025-08-30 19:20:27 +0100

    reduced sidebar width in dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -13,7 +13,12 @@ export default function DashboardLayout({
   children: React.ReactNode
 }) {
   return (
-    <SidebarProvider defaultOpen>
+    <SidebarProvider 
+      defaultOpen
+      style={{
+        "--sidebar-width": "12.5rem",
+      } as React.CSSProperties}
+    >
       <AppSidebar />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
